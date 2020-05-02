c46859da6adc188128a61621c4eb2ec237048a3b
Alexander Wende <alexanderwende@users.noreply.github.com>
2020-05-02 16:44:43 +0200

    fix: make checkboxes work like arrays

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,34 @@ import './elements/radio-button';
 async function bootstrap () {
 
     console.log('loaded..');
+
+    const form = document.getElementById('survey') as HTMLFormElement;
+    const submit = document.getElementById('submit') as HTMLButtonElement;
+
+    submit!.addEventListener('click', () => {
+
+        const formJSON = [...new FormData(form).entries()].reduce((json, [name, value]) => {
+
+            if (json.hasOwnProperty(name)) {
+
+                if (!(json[name] instanceof Array)) {
+
+                    json[name] = [json[name]];
+                }
+
+                json[name].push(value);
+
+            } else {
+
+                json[name] = value;
+            }
+
+            return json;
+
+        }, {} as { [key: string]: any });
+
+        console.log(formJSON);
+    });
 }
 
 window.addEventListener('DOMContentLoaded', () => {
